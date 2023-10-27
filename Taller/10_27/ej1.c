ba6f2ddfcf1b3347ec6d63d42d230719e8be81a8
manuothatceguy <mothatceguy@itba.edu.ar>
2023-10-27 14:47:40 -0300

    taller 27/10 ejercicio 1 recursividad potencia de 3

---
+++ Taller/10_27/ej1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+ 
+int esPotenciaDe3(int n){
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    if(n % 3 == 0)
+        return esPotenciaDe3(n/3);
+    return 0;
+}
+
+int
+main(void) {
+    assert(esPotenciaDe3(0) == 0);
+    assert(esPotenciaDe3(1) == 1);
+    assert(esPotenciaDe3(3) == 1);
+    assert(esPotenciaDe3(9) == 1);
+    assert(esPotenciaDe3(27) == 1);
+    assert(esPotenciaDe3(30) == 0);
+    assert(esPotenciaDe3(100) == 0);
+    printf("OK!\n");
+    return 0;
+}
